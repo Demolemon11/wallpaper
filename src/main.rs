fed9e3847590c3dd032a57cb2fa7797896ee0372
aaravlu <demolemon@outlook.at>
2024-12-11 21:24:54 +0800

    fix: Compatible all platform's picture dir

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,27 @@ use windows::Win32::UI::WindowsAndMessaging::{
 };
 
 const URL: &str = "https://bing.img.run/uhd.php";
-const PATH: &str = "C:/Users/Administrator/Pictures/Today_Bing_Wallpaper.jpg";
+const PATH_BASE: &str = "/Pictures/Today_Bing_Wallpaper.jpg";
 
 fn main() {
+    let home_path = home::home_dir().unwrap();
+    let home_dir = home_path.to_str().unwrap().to_string();
+    let result_dir = home_dir + PATH_BASE;
+
     while TcpStream::connect("8.8.8.8:53").is_err() {
         thread::sleep(Duration::from_secs(3))
     }
 
     let response = blocking::get(URL).unwrap();
-    let mut file = File::create(PATH).unwrap();
+    let mut file = File::create(&result_dir).unwrap();
     let mut content = io::Cursor::new(response.bytes().unwrap()); // 获取内容并写入
 
     copy(&mut content, &mut file).unwrap();
 
-    let file_path_wide: Vec<u16> = PATH.encode_utf16().chain(std::iter::once(0)).collect();
+    let file_path_wide: Vec<u16> = result_dir
+        .encode_utf16()
+        .chain(std::iter::once(0))
+        .collect();
 
     unsafe {
         SystemParametersInfoW(
